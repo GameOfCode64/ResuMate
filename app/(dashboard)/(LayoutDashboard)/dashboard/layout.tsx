5c25fac02d7d4b85c36d2f59e64547e00279b9f5
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2024-07-07 14:23:08 +0530

    fix bug in dashboard layout

--- app/(dashboard)/(LayoutDashboard)/dashboard/layout.tsx
+++ app/(dashboard)/(LayoutDashboard)/dashboard/layout.tsx
@@ -1,51 +1,36 @@
 "use client";
+
 import ResumeModel from "@/components/Create-resuma";
 import ProModal from "@/components/Pro-modal";
 import DashboardSidebar from "@/components/Sidebar";
 import DashbordTopNav from "@/components/dashbord-top-nav";
 import DashboardModialNav from "@/components/mobile-navbar";
-import { Metadata } from "next";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-// Matadata
-// export const metadata: Metadata = {
-//   title: "ResumaAi | Dashboard",
-//   description: "MyResumeHub is Modern Resume builder AI",
-// };
+interface LayoutProps {
+  children: React.ReactNode;
+  shownav?: boolean; // made optional with default value
+  showsidebar?: boolean; // made optional with default value
+}
 
-const layout = ({
+const Layout: React.FC<LayoutProps> = ({
   children,
   shownav = false,
   showsidebar = false,
-}: {
-  children: React.ReactNode;
-  shownav: boolean;
-  showsidebar: boolean;
 }) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const path = usePathname();
 
-  if (path === "/dashboard") {
-    shownav = true;
-  }
-  if (path === "/dashboard/resume") {
-    shownav = true;
-  }
-  if (path === "/dashboard/cover-letters") {
-    shownav = true;
-  }
-  if (path === "/dashboard/resignation-letters") {
-    shownav = true;
-  }
   if (
     path === "/dashboard" ||
     path === "/dashboard/resume" ||
     path === "/dashboard/cover-letters" ||
     path === "/dashboard/resignation-letters"
   ) {
+    shownav = true;
     showsidebar = true;
   }
+
   return (
     <div className="flex items-start justify-normal w-full">
       <DashboardSidebar apiLimiteCount={0} isPro={false} show={showsidebar} />
@@ -60,4 +45,4 @@ const layout = ({
   );
 };
 
-export default layout;
+export default Layout;

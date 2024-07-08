3ae42380bd07ff0b983527691873a4c591aeca27
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2024-07-08 14:05:15 +0530

    ui chnages

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,10 +6,10 @@ const HomePage = () => {
   return (
     <div className="flex items-center justify-center mt-14">
       <div className="flex flex-col items-center justify-center">
-        <h1 className=" text-[68px] text-transparent bg-clip-text bg-gradient-to-r from-purple-400 cursor-pointer transition-all  ease-in-out to-pink-600 font-extrabold mb-4">
+        <h1 className=" md:text-[68px] text-[36px] text-transparent bg-clip-text bg-gradient-to-r from-purple-400 cursor-pointer transition-all  ease-in-out to-pink-600 font-extrabold mb-4">
           Get your Dream job
         </h1>
-        <h2 className="text-[48px]  text-white font-extrabold ">
+        <h2 className="md:text-[48px] text-[28px]  text-white font-extrabold ">
           Using the the power of AI
         </h2>
         <h3 className=" text-[38px] text-transparent bg-clip-text bg-gradient-to-r from-[#9254cc] to-rose-600 font-extrabold mt-2 mb-4">
@@ -21,7 +21,7 @@ const HomePage = () => {
           </Button>
         </Link>
 
-        <div className="mt-32  text-center  pl-3 text-gray-300">
+        <div className="md:mt-32 mt-12  text-center  pl-3 text-gray-300 mb-32">
           ResuMate! AI powered Resume Builder. Using ResuMate build your future,
           get your dream job ❤️
         </div>
